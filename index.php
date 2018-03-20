462bef8a1c35970e27855faf2d6dcca3129b248a
xosrika <gkhos15@freeuni.edu.ge>
2018-03-20 19:39:23 +0400

    Created scratch of Main Page

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-<?php
-	echo "Hello World";
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+	<meta name="author" content="Chris Mills">
+	<meta name="description" content="This is the home page of image gallery site. 
+	Here you can see all kind of photos divided into categories.">
+    <title>Image Gallery</title>
+  </head>
+  <body>
+	<ul>
+		<li>Home Page</li>
+		<li>Categories</li>
+		<li>Log In</li>
+		<li>Sign in</li>
+	</ul>
+	<h1>Home Page</h1>
+	<h2>Description:</h2>
+    <p>This is Image Gallery website. Here you can see all kind of photos divided into categories. 
+	</p>
+  </body>
 
-?>
+
+</html>
