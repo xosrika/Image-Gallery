c99b5b0240bcb584d8df71b92e871128c0fb15fe
xosrika <gkhos15@freeuni.edu.ge>
2018-06-10 21:39:30 +0400

    Added gulp

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Rectangle = function Rectangle(height, width) {
+  _classCallCheck(this, Rectangle);
+
+  this.height = height;
+  this.width = width;
+};
